883674c66244d0a60539fff987e2866c9dda181d
VinhTran <vinhtran144@gmail.com>
2023-08-05 08:55:19 -0400

    Added Yoga with Graphiql

--- server/index.js
+++ server/index.js
@@ -1,29 +1,20 @@
 require('dotenv').config();
 const express = require('express');
-// const createYoga = require('graphql-yoga');
-const port = 5000;
-// const {schema} = require('./schema') ; 
-const makeExecutableSchema = require('@graphql-tools/schema');
+const { createSchema, createYoga } = require('graphql-yoga');
+const port = process.env.PORT || 5000;
+const {typeDefs, resolvers} = require('./schema') ; 
 
-const schema = `
-  type Query {
-    hello: String
-  }
-`;
-
-const executableSchema = makeExecutableSchema({
-  typeDefs: schema,
-  resolvers: {
-    Query: {
-      hello: () => 'Hello, world!',
-    },
-  },
-});
-
-// const graphQLServer  = createYoga({schema});
 const app = express();
 
-// app.use(graphQLServer.graphqlEndpoint(schema));
-// console.log(schema);
+// start a Yoga graghql server
+const graphQLServer = createYoga({
+    schema: createSchema({
+        typeDefs,
+        resolvers
+    }),
+    graphiql: true
+})
+
+app.use(graphQLServer.graphqlEndpoint, graphQLServer);
 
 app.listen(port, console.log(`Server running on port ${port}`));
--- server/schema/index.js
+++ server/schema/index.js
@@ -1,6 +1,5 @@
 // This modulation technique is based featured in https://www.apollographql.com/blog/backend/schema-design/modularizing-your-graphql-schema-code/
 const  { merge } = require('lodash');
-const makeExecutableSchema = require('@graphql-tools/schema');
 
 const { 
     typeDef: Users, 
@@ -15,11 +14,9 @@ const Query = `
     _empty: String
   }
 `;
-const resolvers = {};
 
- const schema = makeExecutableSchema({
-  typeDefs: Query + Users + Links,
-  resolvers: merge(resolvers, usersResolvers, linksResolvers),
-});
+  const typeDefs=  [Query , Users, Links];
+  const resolvers= merge(usersResolvers, linksResolvers);
+
   
-module.exports = schema;
+module.exports = {typeDefs, resolvers};
