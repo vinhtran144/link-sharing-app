2d883079a8e8504409db6384f76279a955079e47
VinhTran <vinhtran144@gmail.com>
2023-08-27 11:08:36 -0400

    tested express sessions storage in mongo

--- server/index.js
+++ server/index.js
@@ -26,8 +26,20 @@ app.use(graphQLServer.graphqlEndpoint, graphQLServer);
 db.once('open',()=>{
     console.log(`Connected to host ${db.host}`);
     app.listen(port, console.log(`Server running on port ${port}`));
-    const store = connectMongo.create({
+    const sessionStore = connectMongo.create({
         client: db.client
     });
-    
+    app.use(session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: sessionStore,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+        }
+    }));
+    app.get('/',(req,res,next)=>{
+        console.log(req.session);
+        res.send('Hello World');
+    })
 });
